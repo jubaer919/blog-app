31951a554aea6c6803d90680cfd9cc189b9f57fe
jubaer919 <60255197+jubaer919@users.noreply.github.com>
2023-12-01 11:12:39 +0600

    added some functionality to resolve the code reviewer issues

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
 
+  after_save :comments_counter
+
   def comments_counter
     post.update(comments_counter: post.comments.count)
   end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,8 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
+  after_save :likes_counter
+
   def likes_counter
     post.update(likes_counter: post.likes.count)
   end
