3eaaff465b40677519be5952dfdf995d5ceaec54
jubaer919 <60255197+jubaer919@users.noreply.github.com>
2023-12-13 14:02:52 +0600

    complete the intregration spec

--- Gemfile
+++ Gemfile
@@ -49,6 +49,8 @@ gem 'rails-controller-testing'
 # gem "image_processing", "~> 1.2"
 
 group :development, :test do
+  gem 'capybara'
+  gem 'webdrivers'
   gem 'rspec-rails'
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri windows]
@@ -67,6 +69,7 @@ end
 
 group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
-  gem 'capybara'
+  # gem 'capybara'
   gem 'selenium-webdriver'
+  # gem 'webdrivers'
 end
---
+++ spec/system/user_show_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :feature do
+  describe 'User show page' do
+    before do
+      @user1 = User.create(name: 'Donia', photo: 'https://via.placeholder.com/150', bio: 'I love coding!')
+      @user1.save
+      @post1 = @user1.posts.create(title: 'Ruby', text: 'This is my second post.')
+      @post2 = @user1.posts.create(title: 'Java', text: 'ccc.')
+      @post3 = @user1.posts.create(title: 'Third post', text: 'This is my third post.')
+    end
+
+    it "displays the user's profile picture" do
+      visit user_path(@user1)
+      expect(page).to have_css("img[src='#{@user1.photo}']")
+    end
+
+    it "displays the user's username" do
+      visit user_path(@user1)
+      expect(page).to have_content(@user1.name)
+    end
+
+    it 'displays the number of posts the user has written' do
+      visit user_path(@user1)
+      expect(page).to have_content(@user1.posts_counter)
+    end
+
+    it "displays the user's bio" do
+      visit user_path(@user1)
+      expect(page).to have_content(@user1.bio)
+    end
+
+    it "displays the user's first 3 posts" do
+      visit user_path(@user1)
+      expect(page).to have_content(@post1.text)
+      expect(page).to have_content(@post2.text)
+      expect(page).to have_content(@post3.text)
+    end
+
+    it 'should render the button that lets view all of a user\'s posts' do
+      visit user_path(@user1)
+      expect(page).to have_css('a.all-post', text: /See All Posts/i, match: :prefer_exact)
+    end
+  end
+end
